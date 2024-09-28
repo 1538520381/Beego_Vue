57a1ba438f8a0d4320fa41631059901fe6e3e32b
Persolute <1538520381@qq.com>
2024-09-28 12:25:36 +0800

    路由

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+const routes = [{
+    path: '/', redirect: '/home',
+}, {
+    path: '/home', name: 'Home', component: () => import('@/views/home/index.vue')
+}]
+
+const router = createRouter({
+    history: createWebHistory(), routes: routes
+})
+
+export default router

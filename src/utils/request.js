15ae2f21f829fc51f953db481739bb4728b9355b
Persolute <1538520381@qq.com>
2024-10-23 11:02:41 +0800

    设置超时限制为50秒

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: '/api', timeout: 5000,
+    baseURL: '/api', timeout: 50000,
 })
 
 request.interceptors.request.use(config => {
